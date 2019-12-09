756acc17409e88f960c29b2dc5aa2ec0c9bb30f4
lesore0789 <lesore0789@gmail.com>
2019-12-08 20:32:51 -0800

    Add pages and set up routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
-
-// import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  return <>hello from app</>;
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
