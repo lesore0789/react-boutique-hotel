3ca06b2c0256836c3ed8985cffd7c94eb127d9a2
lesore0789 <lesore0789@gmail.com>
2019-12-16 20:22:43 -0800

    Set up Rooms Container component with HOC

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,25 +1,49 @@
 import React from "react";
 import RoomsFilter from "./RoomsFilter";
 import RoomsList from "./RoomsList";
-import { RoomConsumer } from "../context";
+import { withRoomConsumer } from "../context";
 import Loading from "./Loading";
 
-export default function RoomsContainer() {
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
   return (
-    <RoomConsumer>
-      {value => {
-        const { loading, sortedRooms, rooms } = value;
-        if (loading) {
-          return <Loading />;
-        }
-        return (
-          <div>
-            hello from rooms container
-            <RoomsFilter rooms={rooms} />
-            <RoomsList rooms={sortedRooms} />
-          </div>
-        );
-      }}
-    </RoomConsumer>
+    <>
+      <RoomsFilter rooms={rooms} />
+      <RoomsList rooms={sortedRooms} />
+    </>
   );
 }
+
+export default withRoomConsumer(RoomContainer);
+
+// This is done with a HOC. Below you can see how to do it without an HOC.
+// The HOC is connected to the withRoomConsumer function in the context file
+
+// import React from "react";
+// import RoomsFilter from "./RoomsFilter";
+// import RoomsList from "./RoomsList";
+// import { RoomConsumer } from "../context";
+// import Loading from "./Loading";
+
+// export default function RoomsContainer() {
+//   return (
+//     <RoomConsumer>
+//       {value => {
+//         const { loading, sortedRooms, rooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+//         return (
+//           <div>
+//             hello from rooms container
+//             <RoomsFilter rooms={rooms} />
+//             <RoomsList rooms={sortedRooms} />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// }
