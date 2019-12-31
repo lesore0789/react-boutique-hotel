c8cb743d3a92d5f323b673448ed00bc4fbcbaba7
lesore0789 <lesore0789@gmail.com>
2019-12-30 16:09:38 -0800

    Filter rooms by type

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,5 +1,59 @@
 import React from "react";
+import { useContext } from "react";
+import { RoomContext } from "../context";
+import Title from "../components/Title";
 
-export default function RoomsFilter() {
-  return <div>hello form rooms filter</div>;
+// Get all Unique Values
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))];
+};
+export default function RoomsFilter({ rooms }) {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
+
+  // Get Unique Types
+  let types = getUnique(rooms, "type");
+  // Add All
+  types = ["all", ...types];
+  // Map to JSX
+  types = types.map((item, index) => {
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+
+  return (
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form className="filter-form">
+        {/* Select Type */}
+        <div className="form-group">
+          <label htmlFor="type">Room type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+        {/*End of Select Type  */}
+      </form>
+    </section>
+  );
 }
